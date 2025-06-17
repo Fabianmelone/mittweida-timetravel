636e9ee4bc5800758af4ec98fe6fcc2a8b4fe0c5
Fabi <fabianmelone.fw@gmail.com>
2025-06-17 21:14:06 +0200

    create a route to redirect to new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import LoadingScreen from "./assets/components/LoadingScreen.tsx";
+import LoadingScreen from "./assets/components/loadingScreen/LoadingScreen.tsx";
 import {useEffect, useState} from "react";
 import WelcomePage from "./assets/components/WelcomePage.tsx";
+import MainPage from "./assets/components/MainPage.tsx";
+import {Route, useLocation} from "wouter";
+
 
 function App() {
+    const [location, setLocation] = useLocation();
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
+    const timer = setTimeout(() => setLoading(false), 2000);
     return () => clearTimeout(timer); // Cleanup
   }, []);
 
+  useEffect(() => {
+    const hasVisited = localStorage.getItem("hasVisited");
+
+    if (!hasVisited && location === "/") {
+        localStorage.setItem("hasVisited", "true");
+        setLocation("/welcome")
+    }
+    }, [location, setLocation]);
+
+  if (loading) return <LoadingScreen />;
+
+
+
   return (
       <>
-        {loading ? <LoadingScreen /> :
-            <div>
-              <WelcomePage />
-            </div>}
+        <Route path="/welcome" component={WelcomePage} />
+        <Route path="/" component={MainPage} />
       </>
-
-  )
+  );
 }
 
 export default App;
--- src/assets/components/WelcomePage.tsx
+++ src/assets/components/WelcomePage.tsx
@@ -1,14 +1,24 @@
 import WelcomeText from "./WelcomeText.tsx";
 import ImageSlider from "./sliderModule/ImageSlider.tsx";
+import {useLocation} from "wouter";
 
 export default function WelcomePage() {
+    const [, setLocation] = useLocation();
 
     return (
         <div className="welcome-page">
 
-            <ImageSlider imageOne={'../public/img/mittweida-now-2020.png'} imageTwo={"../public/img/mittweida-before-1914.png"} />
+            <ImageSlider
+                imageOne={'../public/img/mittweida-now-2020.png'}
+                imageTwo={"../public/img/mittweida-before-1914.png"} />
             <WelcomeText />
-            <button className="start-button">Start Exploring</button>
+            <button
+                className="start-button"
+                onClick={() => setLocation("/")}
+                aria-label="Start Exploring"
+            >
+            Start Exploring
+            </button>
         </div>
-    )
+    );
 }
