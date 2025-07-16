8c07d0a3989fd6d884afde87335a599ae204c882
Fabi <fabianmelone.fw@gmail.com>
2025-07-16 14:36:58 +0200

    replace localhost with ip address2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://fabianmelone.github.io'],
+    origin: [
+      'http://localhost:5173',
+      'https://fabianmelone.github.io',
+      'http://172.17.214.162:5173',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
