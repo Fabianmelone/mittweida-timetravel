322cb0a3c8b70542ef6c3c274afcc288b7abecfd
Fabi <fabianmelone.fw@gmail.com>
2025-07-16 23:43:31 +0200

    add swagger openapi docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,18 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Mittweida Time Travel')
+    .setDescription(
+      'Mittweida Location Post and Receive API, all the Locations that you visited, will be saved and returned with this API.',
+    )
+    .setVersion('1.0')
+    .addTag('Location')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000, '0.0.0.0');
 }
 
